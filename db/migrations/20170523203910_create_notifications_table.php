bbd308fada312ac10fc45b3521a50a4a54f3902c
Radu Bordei <radu.bordei89@gmail.com>
2017-05-23 23:42:33 +0300

    adding notifications migration

---
+++ db/migrations/20170523203910_create_notifications_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class CreateNotificationsTable extends AbstractMigration {
+
+    /**
+     * Create notifications table
+     *
+     */
+    public function up() {
+
+    }
+
+    /**
+     * Remove notifications table .
+     *
+     */
+    public function down() {
+
+    }
+}
